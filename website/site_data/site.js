60038bf492f290e460a9651e2d53d57670cccc89
M. Elijah Wangeman <maw4678@rit.edu>
2026-03-02 15:24:02 -0500

    production url var enough to fix?

--- website/site_data/site.js
+++ website/site_data/site.js
@@ -1,4 +1,4 @@
 export default {
     // prepend this to the site-local path to get full absolute URLs
-    production_url: 'https://example.com'
+    production_url: 'https://mindcat.github.io/zoodb-q-routing'
 };
